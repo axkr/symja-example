a40e6cd9bd672262f62af69fdb96b439534936d3
duytran <duytranle.it@gmail.com>
2023-06-21 00:54:48 +0700

    Added Android integration test

--- app/src/main/java/com/symja/app/SymjaApplication.java
+++ app/src/main/java/com/symja/app/SymjaApplication.java
@@ -2,6 +2,13 @@ package com.symja.app;
 
 import android.app.Application;
 
+import org.matheclipse.core.basic.AndroidLoggerFix;
+
 public class SymjaApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidLoggerFix.fix();
+    }
 }
