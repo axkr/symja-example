e40864886eeef77cbbcc5b6ebeb04dfa28adfbc2
duytran <duytranle.it@gmail.com>
2023-10-05 19:15:47 +0700

    AppAnalytics hook

--- modules/common/src/main/java/com/symja/common/analyst/AppAnalytics.java
+++ modules/common/src/main/java/com/symja/common/analyst/AppAnalytics.java
@@ -7,12 +7,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class AppAnalytics {
-    public static AppAnalytics getInstance(Context context) {
-        return new AppAnalytics();
+
+    @Nullable
+    public static OnLogEvent action;
+
+    private final Context context;
+
+    public AppAnalytics(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    public static AppAnalytics getInstance(@NonNull Context context) {
+        return new AppAnalytics(context);
     }
 
     public void logEvent(@NonNull String event, @Nullable Bundle bundle) {
+        if (action != null) {
+            action.logEvent(context, event, bundle);
+        }
     }
+
     public void logEvent(@NonNull String event) {
+        if (action != null) {
+            action.logEvent(context, event, null);
+        }
+    }
+
+    public interface OnLogEvent {
+        void logEvent(Context context, String event, @Nullable Bundle bundle);
     }
 }
